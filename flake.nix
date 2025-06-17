46990eea1ae7c43e301da992e3b54b3c12ffed7a
lessuseless <179788364+lessuselesss@users.noreply.github.com>
2025-06-17 01:02:57 -0600

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,4 +1,58 @@
 {
+  description = "A Nix flake for dnsproxy with dynamic DNS configurations for apps";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        # Import the dnsproxy package
+        dnsproxy = import ./providers/package.nix { inherit pkgs; };
+
+        # Wrapper script to dynamically handle arguments
+        dnsProxyScript = pkgs.writeShellScript "dns-proxy-runner" ''
+          # Parse arguments
+          AppBinaryPackageName="$1"
+          Provider="$2"
+          Command="$3"
+          Action="$4"
+          Interface="$5"
+          Options="$6"
+
+          # Example: Set DNS provider for app
+          if [ "$Action" = "set" ]; then
+            exec ${dnsproxy}/bin/dnsproxy --upstream dns://$Provider --interface $Interface $Options "$AppBinaryPackageName"
+          elif [ "$Action" = "get" ]; then
+            echo "Getting DNS configuration for $AppBinaryPackageName on $Interface"
+            # Placeholder for actual `get` implementation
+          else
+            echo "Invalid action: $Action"
+            exit 1
+          fi
+        '';
+
+      in
+      {
+        packages = {
+          default = dnsproxy;
+        };
+
+        apps = {
+          dns-proxy = {
+            type = "app";
+            program = "${dnsProxyScript}";
+          };
+        };
+      }
+    );
+}
+
+}
   description = "A Nix flake for dnsproxy with encrypted DNS providers";
 
   inputs = {
